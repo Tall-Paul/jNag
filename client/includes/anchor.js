0f0550276812d53229975cb8b17f47da7d9fe751
Tall-Paul <tall_paulb@hotmail.com>
2011-02-04 10:42:47 +0000

    removed old client directory

--- client/includes/anchor.js
+++
@@ -1,9 +0,0 @@
-
-    //Grab our current Url
-	   var url = window.location.toString();
-	 //Remove anchor from url
-	 var anchor_index = url.indexOf('#');
-    if (anchor_index != -1) {
-    	 window.location = url.substring(0, anchor_index);
-    }
-    
