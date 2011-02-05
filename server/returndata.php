9660c2fc9c4705c433a2a15dff489fe78e5a8599
PaulB <tall-paulb@hotmail.com>
2011-02-05 20:14:05 +0000

    added cache control headers to returndata to try and prevent caching

--- server/returndata.php
+++ server/returndata.php
@@ -4,6 +4,13 @@
     if (!isset($data_address))
         include("config.php");
         
+    
+    // PREVENT CACHING FIRST BEFORE ANYTHING ELSE!
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // alwaysmodified
+    header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache"); // HTTP/1.0
     //CORS headers, taken from http://saltybeagle.com/2009/09/cross-origin-resource-sharing-demo/
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
