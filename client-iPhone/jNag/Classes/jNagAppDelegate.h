57dbd54efc01c38fcb7a0916e5dfa50beb586353
Tall-Paul <tall_paulb@hotmail.com>
2011-03-20 16:49:43 +0000

    iPhone app added

---
+++ client-iPhone/jNag/Classes/jNagAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  jNagAppDelegate.h
+//  jNag
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface jNagAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ client-iPhone/jNag/www/includes/platform.js
@@ -0,0 +1,10 @@
+//settings
+var jNag_platform = {
+      phonegap: true, //load phonegap library
+      phonegap_settings: false,
+      ads: true,      //load advertising library
+      footer: true,    //show footers
+      version: "1.8.1"  //version string
+};
+
+
